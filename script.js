21fdc676d3d3c81642e39b667f5736b8511712a6
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-18 17:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+ function updateOrientation() {
+    const orientation = window.orientation;
+
+    if (orientation === 0 || orientation === 180) {
+      document.body.style.backgroundImage  = `url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')`;
+    } else {
+      document.body.style.backgroundImage  = `url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')`;
+    }
+  }
+
+  // Initial check and update
+  updateOrientation();
+
+  // Add a listener for changes in orientation
+  window.addEventListener('orientationchange', updateOrientation)
