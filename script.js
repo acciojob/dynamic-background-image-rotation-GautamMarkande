2212aa1490f529be3d1770e1d7e239d89406aed3
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-18 18:02:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  function updateOrientation() {
     const orientation = window.orientation;
 
-    if (orientation === 0 || orientation === 180) {
+    if (orientation === 0 || orientation >= 180) {
       document.body.style.backgroundImage  = `url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')`;
     } else {
       document.body.style.backgroundImage  = `url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')`;
